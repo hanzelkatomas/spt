d6240b9f0d431548eebdcd606b57a34970effe5d
hanzelkatomas <49791145+hanzelkatomas@users.noreply.github.com>
2019-04-19 18:25:55 +0200

    Add files via upload

---
+++ Projekt_SPT/Projekt_SPT/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_SPT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string pokracovat;
+            bool ok;
+            int x;
+            Console.WriteLine("Prevodnik v0.1\n--------------\n");
+            
+
+            do
+            {
+                Console.WriteLine("Zadejte cislo, ktere chcete prevest:");
+                do
+                {                    
+                    ok = int.TryParse(Console.ReadLine(), out x);
+                    if (!ok) Console.WriteLine("Musite zadat cislo!");
+                } while (!ok);
+                
+                
+                Console.WriteLine("Zvolte soustavu do ktere chcete cislo prevest:\nHexadecimalni - 16\nOsmickova - 8\nBinarni - 2");
+                string soustava = Console.ReadLine();
+            ;
+                switch (soustava)
+                {
+                    case "2":
+
+                        double preVelikostPoleBin;
+                        int hodnotaBin = x;
+                        if (hodnotaBin != 0)
+                        {
+                            preVelikostPoleBin = Math.Floor(Math.Log(x, 2));                           
+                        }
+                        else
+                        {
+                            preVelikostPoleBin = 0;                            
+                        }
+                        int binVelikostPole = (int)preVelikostPoleBin;
+                        char[] charArrayBin = new char[binVelikostPole + 1];
+
+                        for (int i = 0; i <= binVelikostPole; i++)
+                        {
+                            if ((hodnotaBin % 2 == 1) && (i < binVelikostPole))
+                            {
+                                charArrayBin[i] = '1';
+                                hodnotaBin /= 2;
+                            }
+                            else if ((hodnotaBin % 2 == 0) && (i == binVelikostPole))
+                            {
+                                charArrayBin[i] = '0';
+                            }
+                            else if ((hodnotaBin % 2 == 1) && (i == binVelikostPole))
+                            {
+                                charArrayBin[i] = '1';
+                            }
+                            else
+                            {
+                                charArrayBin[i] = '0';
+                                hodnotaBin /= 2;
+                            }
+                        }
+
+                        Array.Reverse(charArrayBin);
+                        Console.WriteLine($"Cislo {x} v binarni soustave: ");
+                        foreach (char prvek in charArrayBin)
+                        {
+                            Console.Write($"{prvek} ");
+                        }
+                        break;                   
+
+
+                    default:
+                        Console.WriteLine("\nMusite zadat 2, 8 nebo 16 pro prevod do dane soustavy!");
+                        break;
+                }
+
+                Console.WriteLine("\n\nPokud chcete pokracovat napiste ano, v jinem pripade bude program ukoncen.");
+               
+            } while((pokracovat = Console.ReadLine()) == "ano");             
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
---
+++ Projekt_SPT/Projekt_SPT/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("Projekt_SPT")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Projekt_SPT")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// modelu COM, nastavte atribut ComVisible daného typu na hodnotu True.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("11de0d97-45d4-4417-a332-fb06fc953051")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+// Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize pomocí *, jak je vidět níže:
+// pomocí zástupného znaku * takto:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
